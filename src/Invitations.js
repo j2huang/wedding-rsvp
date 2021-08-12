eaa8a615aab243afab8af58fdebdc16fe5f14e2a
Jenny Huang <jenny.huang@jennyhuang-ltm.internal.salesforce.com>
2021-08-11 18:47:54 -0700

    second attempt fix at typeform modal

--- src/Invitations.js
+++ src/Invitations.js
@@ -1,8 +1,6 @@
 function Invitations() {
     const myHTML = `
-    <div style="text-align:center;"> 
-    <button data-tf-popup="FOdVBYAG" style="all:unset;font-family:Helvetica,Arial,sans-serif;display:inline-block;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;background-color:#FBCE37;font-size:20px;border-radius:25px;color:#fff;padding:0 33px;font-weight:bold;height:50px;cursor:pointer;line-height:50px;text-align:center;margin:0;text-decoration:none;">RSVP Here</button><script src="//embed.typeform.com/next/embed.js"></script>
-    </div>
+    <div style="text-align:center;"> <a class="typeform-share button" href="https://form.typeform.com/to/FOdVBYAG?typeform-medium=embed-snippet" data-mode="popup" style="display:inline-block;text-decoration:none;background-color:#FBCE37;color:#0B2826;cursor:pointer;font-family:Helvetica,Arial,sans-serif;font-size:20px;line-height:50px;text-align:center;margin:0;height:50px;padding:0px 33px;border-radius:25px;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:bold;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;" data-size="100" target="_blank">RSVP Here </a> </div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script>
     `;
   
     return (
